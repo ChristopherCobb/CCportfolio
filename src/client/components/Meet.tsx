b325920dc69410d07634cfa5582cc96dd7487ee2
Christopher <cbcobb4@gmail.com>
2021-02-01 19:14:37 -0600

    added new assets - styling changes

---
+++ src/client/components/Meet.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../scss/app.scss"
+
+
+
+const Meet = () => {
+  return (
+    <div className="meetcontent">
+      <img
+        className="meetimage"
+        src="https://i.imgur.com/laAGjHj.jpg"
+        alt="christopher's headshot"
+      />
+      <h6 className="descriptions">
+        <a href="https://drive.google.com/file/d/1mahVWLWJhYzvnhymwsCzwbl-7A6xR-l9/view?usp=sharing">
+          <h6 className="resume">Resume</h6>
+        </a>
+        <br />
+        Developer + more info  
+         <br />
+        <br /> Insert information about myself here.  <br />
+        <br />
+      Insert Info here <br />
+        <br />
+        I've spent the last 10 years as a chef in multiple roles.
+        <br />
+        I'm a full stack developer who most enjoys working with React, node.js,
+        ExpressJS, and mySQL.
+        <br />
+        <br />
+        My name is Christopher, and I look forward to bringing your vision to life.
+      </h6>
+    </div>
+  );
+};
+
+export default Meet;
